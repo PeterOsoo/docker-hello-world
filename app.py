ff1e417ca15d54815fbb5f7a6329efaaa8a797bb
PeterOsoo <posoo617@gmail.com>
2025-08-08 22:58:36 +0300

    title

--- app.py
+++ app.py
@@ -3,7 +3,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_docker():
-    return '<h1>Hello again, from my updated containerized Python app!</h1>'
+    return """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Hello World</title>
+    </head>
+    <body>
+        <h1>Hello again, from my updated containerized Python app!</h1>
+    </body>
+    </html>
+    """
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
